f66802106b1745e7ab1238e204600c7d1edbbb58
hatembellaaj <gitlab-mdsoft@mdsoft-int.com>
2021-04-24 21:44:20 +0100

    Initial version of hello generated by JHipster-6.10.5

---
+++ src/main/java/tn/mdweb/hello/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package tn.mdweb.hello.web.rest;
---
+++ src/test/java/tn/mdweb/hello/ArchTest.java
@@ -0,0 +1,29 @@
+package tn.mdweb.hello;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("tn.mdweb.hello");
+
+        noClasses()
+            .that()
+                .resideInAnyPackage("tn.mdweb.hello.service..")
+            .or()
+                .resideInAnyPackage("tn.mdweb.hello.repository..")
+            .should().dependOnClassesThat()
+                .resideInAnyPackage("..tn.mdweb.hello.web..")
+        .because("Services and repositories should not depend on web layer")
+        .check(importedClasses);
+    }
+}
